f8432290fd691167a430ba58bc2840135742de1a
Louiesaldy Nismal <116267337+Louienismal4@users.noreply.github.com>
2026-07-19 20:25:54 +0800

    infra: add Terraform development foundation

---
+++ infra/environments/development/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region for Summitodoro development infrastructure."
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_region" {
+  value = data.aws_region.current.region
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
